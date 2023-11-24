dcc0fa436039419a09d1c259a66720d786c333b1
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-24 16:45:16 +0200

    Upload files

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\UserProfile;
+use App\Form\ProfileImageType;
 use App\Form\UserProfileType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class SettingsProfileController extends AbstractController
 {
@@ -42,6 +45,50 @@ class SettingsProfileController extends AbstractController
         }
 
         return $this->render('settings_profile/profile.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/settings/profile-image', name: 'app_settings_profile_image')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profileImage(Request $request, SluggerInterface $slugger, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProfileImageType::class);
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $profileImageFile = $form->get('profileImage')->getData();
+
+            if($profileImageFile) {
+                $originalFileName = pathinfo($profileImageFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFileName = $slugger->slug($originalFileName);
+
+                $newFileName = $safeFileName.'-'.uniqid().'.'.$profileImageFile->guessExtension();
+
+                try {
+                    $profileImageFile->move($this->getParameter('profiles_directory'), $newFileName);
+                } catch (FileException $e) {}
+
+                $profile = $user->getUserProfile() ?? new UserProfile();
+
+                $profile->setImage($newFileName);
+                $user->setUserProfile($profile);
+
+                $entityManager->persist($profile);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'The profile image have been updated!');
+
+                return $this->redirectToRoute('app_settings_profile_image');
+            }
+        }
+
+        return $this->render('settings_profile/profile_image.html.twig', [
             'form' => $form,
         ]);
     }
---
+++ src/Form/ProfileImageType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+
+class ProfileImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('profileImage', FileType::class, [
+                'label' => 'Profile Image (JPG or PNG file type)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid JPG or PNG image',
+                    ]),
+                ],
+                'attr' => [
+                    'placeholder' => 'Choose file',
+                    'class' => 'form-control',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
