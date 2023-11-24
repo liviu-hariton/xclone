5d2a4f02f22fc1c92d24c2719313afac569290cc
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-24 11:59:16 +0200

    User profile page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/profile/{id}', name: 'app_profile')]
+    public function show(User $user): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
