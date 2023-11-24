db10441dcefd4d2f07bac1b4cffaf1f649bcc2c5
Liviu Hariton <liviu.hariton@gmail.com>
2023-11-24 14:55:43 +0200

    Profile detail with follows / followers

---
+++ src/Controller/FollowerController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class FollowerController extends AbstractController
+{
+    #[Route('/follow/{id}', name: 'app_follow')]
+    #[isGranted('IS_AUTHENTICATED_FULLY')]
+    public function follow(User $userToFollow, ManagerRegistry $doctrine, Request $request): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if($currentUser->getId() !== $userToFollow->getId()) {
+            $currentUser->follow($userToFollow);
+
+            $doctrine->getManager()->flush();
+        }
+
+        $this->addFlash('info', sprintf('You are now following %s', $userToFollow->getEmail()));
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    #[Route('/unfollow/{id}', name: 'app_unfollow')]
+    #[isGranted('IS_AUTHENTICATED_FULLY')]
+    public function unfollow(User $userToUnfollow, ManagerRegistry $doctrine, Request $request): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if($currentUser->getId() !== $userToUnfollow->getId()) {
+            $currentUser->unfollow($userToUnfollow);
+
+            $doctrine->getManager()->flush();
+        }
+
+        $this->addFlash('info', sprintf('You are now following %s', $userToUnfollow->getEmail()));
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
